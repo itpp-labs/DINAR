acf0085a6aa160fe9a946b5ad8a4b573448b8816
Ivan Yelizariev <yelizariev@it-projects.info>
2019-12-16 16:37:06 +0500

    branch2odoo_version.py

---
+++ workflows/tools/branch2odoo_version.py
@@ -0,0 +1,33 @@
+# reads branch name from input
+# prints odoo version to output
+import fileinput
+
+ODOO_VERSIONS = [
+    "master",
+    "13.0",
+    "12.0",
+    "11.0",
+    "10.0",
+    "9.0",
+    "8.0",
+    "7.0"
+]
+
+
+def branch2version(branch):
+    branch = branch.rstrip()
+    if branch in ODOO_VERSIONS:
+        return branch
+
+    for v in ODOO_VERSIONS:
+        if branch.startswith("%s-" % v):
+            return v
+
+    return ODOO_VERSIONS[0]
+
+
+if __name__ == '__main__':
+    for line in fileinput.input():
+        version = branch2version(line)
+        print(version)
+        break
