124a6b36f54eebd000dd5c16fd4a092cdf960015
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-05 18:14:10 +0500

    revert colors

--- workflow-files/fork2repos.py
+++ workflow-files/fork2repos.py
@@ -87,7 +87,7 @@ def dir_is_empty(path):
     return len(os.listdir(path)) == 0
 
 def cmd(command):
-    print(colors.green & colors.bold | str(command))
+    print(command)
     print(command())
 
 
