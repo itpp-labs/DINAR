7f11f4e38d530931e71a1f78be674c187430853f
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-06 08:50:12 +0500

    fix "ValueError: I/O operation on closed file."

--- workflow-files/images-with-preinstalled-modules.sh
+++ workflow-files/images-with-preinstalled-modules.sh
@@ -4,7 +4,7 @@ docker-compose -p DINAR -f $DIR/docker-compose.yml config
 docker-compose -p DINAR -f $DIR/docker-compose.yml up -d
 # this fixes the input device is not a TTY .. see https://github.com/docker/compose/issues/5696
 export COMPOSE_INTERACTIVE_NO_CLI=1
-docker-compose -p DINAR -f $DIR/docker-compose.yml exec odoo odoo -i $MODULES --stop-after-init
+docker-compose -p DINAR -f $DIR/docker-compose.yml exec -T odoo odoo -i $MODULES --stop-after-init
 docker-compose -p DINAR -f $DIR/docker-compose.yml stop
 
 docker commit $(docker inspect --format="{{.Id}}" dinar_db_1) $REGISTRY/$IMAGE_DB
