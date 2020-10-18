4d3295865e6a8a7b3d4dc00c4d055b0549269405
Ivan Yelizariev // IEL <yelizariev@itpp.dev>
2020-10-18 17:06:01 +0200

    Update analyze_port_trigger.py

--- workflow-files/analyze_port_trigger.py
+++ workflow-files/analyze_port_trigger.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
+import sys
 
 # TODO: rename analyze-modules.py file
 import importlib   
